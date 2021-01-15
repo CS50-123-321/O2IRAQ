50eb11b736423917c3f18eb2d9556bd5f00ec2c7
ALFA <123muntadher972GMAIL.COM>
2021-01-15 14:46:08 +0300

    Muntadher- All project - pictures add feature has been added.

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from my import login_required
 from flask_fontawesome import FontAwesome
 #hello m
 import os
+from PIL import Image
 import datetime as dt
 #he
 app = Flask(__name__)
@@ -24,20 +25,27 @@ app.config["SESSION_PERMANENT"] = False
 app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 
-"""@app.route("/o", methods=["GET", "POST"])
+@app.route("/d", methods=["GET", "POST"])
 def jj():
     if request.method == "POST":
         image = request.files["image"]
-        image = image.save(image.filename)
+        path = "C:/Users/ALFA/PycharmProjects/O2-IRAQ/static/pic/"
+        imaged = image.save(path+image.filename)
+        pic_insert = db.execute("insert into user_activity(post_pic) values (:image)", image=image.filename)
+        return redirect("/a")
+    else:
         return render_template("tst.html")
 
-    else:
-        return render_template("tst.html")"""
-
-
+@app.route("/a", methods=["GET", "POST"])
+def jjj():
+    pic_call = db.execute("select post_pic from user_activity where user_id = 12")
+    print(pic_call)
+    return render_template("tstt.html", pic_call=pic_call[0]["post_pic"])
+#<img src="{{ url_for('static', filename='pic/' + pic_call )}}">
+#src="{{ url_for('static', filename='pic/' + pic_insert }}"
 
 @app.route("/", methods=["GET", "POST"])
-def getin():
+def log_in():
     """Homepage"""
     session.clear()
     if request.method == "POST":
@@ -63,24 +71,35 @@ def getin():
 @login_required
 def home():
     """Homepage"""
-    """Current_user = db.execute("select user_name from register where user_id = :user_id", user_id=session["user_id"])
-    current_user = current_user[0]["user_name"]
-    current_user = current_user.split()[0]
-    current_user = current_user.capitalize()"""
-    """image = request.files["myfile"]
-    # image = image.save(image.filename)
-    #pic_insert = db.execute("insert into register(profile_pic) values (:image)", image=image.filename)
-    print ("--------------------------------->", pic_insert)"""
+
     current_user = db.execute("select user_name from register where user_id = :user_id", user_id=session["user_id"])
-    pic_call = db.execute("select profile_pic from register where user_name=:user_name", user_name = current_user[0]["user_name"])
+    pic_call = db.execute("select post_pic from user_activity where user_name=:user_name ORDER BY post_pic DESC LIMIT 1", user_name = current_user[0]["user_name"])
+    if pic_call:
+        pic_call = pic_call[0]["post_pic"]
+    else:
+        pic_call = db.execute(
+            "select post_pic from user_activity where user_name=:user_name ORDER BY post_pic DESC LIMIT 1",
+            user_name=17)
+        user_profile_pic = pic_call[0]["profile_pic"]
+
+
     print("home:---------------------------------->",pic_call)
+    user_profile_pic = db.execute("select profile_pic from register where user_id = :user_id", user_id=session["user_id"])
+
+    if user_profile_pic:
+        user_profile_pic = user_profile_pic[0]["profile_pic"]
+    else:
+        user_profile_pic = db.execute("select profile_pic from register where user_id = :user_id",
+                                      user_id=session["user_id"])
+        user_profile_pic = user_profile_pic[0]["profile_pic"]
+
+    """Posts and Notifications"""
     posts = db.execute("select * from user_activity where user_id != 0")
     posts.reverse()
-
     noti_posts = db.execute("select * from user_activity where user_id != 0 and user_id != :user_id ORDER BY post DESC LIMIT 30",user_id=session["user_id"])
     noti_posts.reverse()
-    #return render_template("home.html", noti_posts = noti_posts)
-    return render_template("home.html", posts=posts, noti_posts=noti_posts,pic_call = pic_call[0]["profile_pic"])
+
+    return render_template("home.html", posts=posts, noti_posts=noti_posts, pic_call = pic_call, profile_pic= user_profile_pic)
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -139,10 +158,24 @@ def logout():
     session.clear()
     return redirect("/")
 
+@app.route("/uploading_pic", methods=["GET", "POST"])
+@login_required
+def personal_pic():
+
+    image = request.files["profile_pic"]
+    print("profile>>>>>>>>>>>>>>>>>>>>>>>profile>>>>>>>>>>>>>profile>>",image)
+    path = "C:/Users/ALFA/PycharmProjects/O2-IRAQ/static/profile_pictures/"
+    image1 = image.save(path + image.filename)
+    counter_update = db.execute("UPDATE register SET profile_pic = :image where user_id = :user_id",
+                                image=image.filename, user_id=session["user_id"])
+    #profile_pic_insert = db.execute("insert into register(profile_pic) values (:profile_pic)", profile_pic = image.filename)
+    return redirect("/profile")
+
 @app.route("/profile", methods=["GET", "POST"])
 @login_required
 def profile():
     user_info = db.execute("select * from register where user_id = :user_id", user_id=session["user_id"])
+    profile_pic = user_info[0]['profile_pic']
     user_name = user_info[0]['user_name']
     user_name = user_name.capitalize()
     user_city = user_info[0]['city']
@@ -157,7 +190,7 @@ def profile():
                                 user_id=session["user_id"])"""
 
     #return render_template("profile.html", user_name=user_name, user_city=user_city, user_bd=user_bd, posts=posts,counter_insert=counter_insert[0]["max(post_counter)"])
-    return render_template("profile.html", user_name=user_name, user_city=user_city, user_bd=user_bd, posts=posts,user_sex= user_sex, user_email=user_email, user_job=user_job)
+    return render_template("profile.html", user_name=user_name, user_city=user_city, user_bd=user_bd, posts=posts,user_sex= user_sex, user_email=user_email, user_job=user_job, profile_pic = profile_pic)
 
 @app.route("/activity", methods=["GET", "POST"])
 @login_required
@@ -166,15 +199,15 @@ def activity():
         return render_template("activity.html")
     else:
         content = request.form.get("text_area")
-        """image = request.files["myfile"]
-        print("activity------------------------>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", image)
-        pic_insert = db.execute("insert into register(profile_pic) values (:image)", image = image.filename)
-        image = image.save('C:/Users/ALFA/PycharmProjects/O2-IRAQ/static/pic/ image.filename', 'JPG')"""
-
+        image = request.files["myfile"]
+        path = "C:/Users/ALFA/PycharmProjects/O2-IRAQ/static/pic/"
+        image1 = image.save(path + image.filename)
+        #pic_insert = db.execute("insert into user_activity(post_pic) values (:image)", image = image.filename)
+        #print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@", pic_insert)
         name = db.execute("select user_name from register where user_id = :user_id", user_id=session["user_id"])
         name = name[0]["user_name"]
-        content_insert = db.execute("insert into user_activity (post, user_name, user_id, date)values (:content,:name,:user_id, current_timestamp)",
-            content=content, name=name, user_id=session["user_id"])
+        content_insert = db.execute("insert into user_activity (post, user_name, user_id, date, post_pic)values (:content,:name,:user_id, current_timestamp, :image)",
+            content=content, name=name, user_id=session["user_id"],image = image.filename)
         # consider using forign key
         post_counter = db.execute("SELECT COUNT(post) FROM user_activity WHERE user_id=:user_id",
                                   user_id=session["user_id"])
@@ -185,14 +218,6 @@ def activity():
                                     post_counter=post_counter[0]["COUNT(post)"], user_id=session["user_id"])
         return redirect("/home")
 
-@app.route("/upload", methods=["GET", "POST"])
-def convert_pic():
-    if request.method == "POST":
-        image = request.files["image"]
-        #image = image.save(image.filename)
-        pic_insert = db.execute("insert into register(profile_pic) values (:image)", image = image.filename)
-    else:
-        return render_template('tst.html')
 
 @app.route("/lookup", methods=["GET", "POST"])
 @login_required
@@ -236,3 +261,8 @@ def notification():
     posts.reverse()
     #return render_template("Ins R
     return render_template("noti.html", posts=posts, current_user=current_user)
+
+if __name__ == '__main__':
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(debug=True, host='127.0.0.1')
